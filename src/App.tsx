830a9ba84e9efc2832c0717a40d01e27c32420dc
Miguel Hernanz <migherdcas@gmail.com>
2020-10-26 19:52:59 +0100

    feat(User auth): App routes defined with firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
-import { AuthProvider } from './firebase/auth';
+import { AuthProvider } from './firebase/Auth';
+import Login from './firebase/Login';
+import PrivateRoute from './firebase/PrivateRoute';
+import SignUp from './firebase/SignUp';
 
 function App(): JSX.Element {
   return (
     <AuthProvider>
       <Router>
         <div>
-          <Route exact path="/" component={Home} />
-          {/* <Route exact path="/login" component={Login} /> */}
-          {/* <Route exact path="/signup" component={SignUp} /> */}
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
         </div>
       </Router>
     </AuthProvider>
