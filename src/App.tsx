9523e15f179cd73b504edb0e0eef752c18480395
Miguel Hernanz <migherdcas@gmail.com>
2020-10-27 20:57:46 +0100

    feat(API):  Fetching town list data with axios implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// Constants
+import { API_URL_LIST } from './constants/constants';
 
 // Firebase
 import firebase from 'firebase/app';
@@ -21,8 +25,28 @@ const firestore = firebase.firestore();
 import { SignIn } from './components/SignIn';
 import { MainPanel } from './components/MainPanel';
 
+interface AppStateProps {
+  loading: boolean;
+  data: null | any;
+}
+
 function App(): JSX.Element {
   const [user] = useAuthState(auth);
+  const [appState, setAppState] = useState<AppStateProps>({
+    loading: false,
+    data: null,
+  });
+
+  useEffect(() => {
+    setAppState({ ...appState, loading: true });
+    const apiUrl = API_URL_LIST;
+    axios
+      .get(apiUrl)
+      .then(({ data }) => {
+        setAppState({ loading: false, data: data });
+      })
+      .catch((error) => console.log(error));
+  }, [setAppState]);
 
   return (
     <div className="App">
---
+++ src/constants/constants.ts
@@ -0,0 +1,8 @@
+export const API_URL_LIST = 'https://www.el-tiempo.net/api/json/v2/municipios';
+
+/*
+  Lista de municipios:
+    https://www.el-tiempo.net/api/json/v2/municipios
+  Información geográfica y meteorológica de un municipio
+    https://www.el-tiempo.net/api/json/v2/provincias/[CODPROV]/municipios/[ID]
+ */
