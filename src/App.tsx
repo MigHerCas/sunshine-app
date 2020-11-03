9538c450b064670d6dc64267b91e4b0eede40069
Miguel Hernanz <migherdcas@gmail.com>
2020-11-03 23:40:10 +0100

    perf(Folder structure): Components name refactoring.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import axios, { AxiosResponse } from 'axios';
+// import axios, { AxiosResponse } from 'axios';
 
 // Constants
-import { API_URL_LIST } from './constants/constants';
+// import { API_URL_LIST } from './constants/constants';
 
 // Firebase
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import { FirebaseConfig } from './firebase/config';
+// import { FirebaseConfig } from './firebase/config';
 
 // Firebase hooks
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 // Initialize firebase app
-firebase.initializeApp(FirebaseConfig);
+// firebase.initializeApp(FirebaseConfig);
+firebase.initializeApp({
+  apiKey: 'AIzaSyD7CHIT0VpArf16ZJDf8rsvyzQQmVLYJt4',
+  authDomain: 'sunshineapp-725c4.firebaseapp.com',
+  databaseURL: 'https://sunshineapp-725c4.firebaseio.com',
+  projectId: 'sunshineapp-725c4',
+  storageBucket: 'sunshineapp-725c4.appspot.com',
+  messagingSenderId: '251229924882',
+  appId: '1:251229924882:web:f97d0e270ccac7fb9cb2cb',
+  measurementId: 'G-P506GB2BHS',
+});
 
 // Firebase global variables
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+firebase.firestore().settings({ experimentalForceLongPolling: true });
 
 // Utils
-import { convertDataToComboOption } from './utils/dataMapping';
 
 // Types
-import { EuiComboBoxOptionOption } from '@elastic/eui';
 import { Town } from './types/types';
 
 // Components
-import ComboBox from './components/ComboBox/Component';
-import Header from './components/layout/Header/Component';
-import Footer from './components/layout/Footer/Component';
-import Credentials from './components/Credentials/Component';
+import Header from './components/layout/Header/Header';
+import Footer from './components/layout/Footer/Footer';
+import Credentials from './components/Credentials/Credentials';
 import { CardsContainer } from './components/CardsContainer/styles';
-import Card from './components/Card/Component';
-import SignOut from './components/SignOut/Component';
+import Card from './components/Card/Card';
+import SignOut from './components/SignOut/SignOut';
 import { Container } from './components/layout/Container/styles';
 import { Credits } from './components/Credits/styles';
+
+interface Ciudad {
+  nombre: string;
+}
 
 function App(): JSX.Element {
   // Firebase authentication
@@ -46,43 +58,16 @@ function App(): JSX.Element {
 
   // Firestore storage
   const searchesRef = firestore.collection('searches');
-  const query = searchesRef.orderBy('NOMBRE').limit(3);
+  const query = searchesRef.orderBy('ciudad').limit(3);
   const [values] = useCollectionData<Town>(query, { idField: 'id' });
 
   // const [loading, setLoading] = useState<boolean>(false);
-  const [dataFromApi, setDataFromApi] = useState<EuiComboBoxOptionOption[]>([]);
-  const [activeOptions, setActiveOptions] = useState<EuiComboBoxOptionOption[]>(
-    []
-  );
+  const [ciudades, setCiudades] = useState<Ciudad[]>([]);
 
   useEffect(() => {
     // Firestore intiial fetch
-    // We need to convert from CollectionData into EuiComboBoxOptionOption[]
-    values && setActiveOptions(convertDataToComboOption(values));
-    console.log('Values', values);
+    values && setCiudades(ciudades);
   }, [values]);
-
-  useEffect(() => {
-    searchesRef.add({
-      activeOptions,
-    });
-  }, [setActiveOptions]);
-
-  //  Data fetching API data fetch + converts it to type EuiComboBoxOptionOption[]
-  useEffect(() => {
-    // setLoading(true);
-
-    // API
-    const apiUrl = API_URL_LIST;
-    axios
-      .get(apiUrl)
-      .then(({ data }: AxiosResponse<Town[]>) => {
-        // setLoading(false);
-        console.log('Api fetch');
-        setDataFromApi(convertDataToComboOption(data));
-      })
-      .catch((error) => console.log(error));
-  }, []);
 
   return (
     <>
@@ -91,15 +76,18 @@ function App(): JSX.Element {
       <Container>
         {user ? (
           <>
-            <ComboBox
-              comboBoxData={dataFromApi}
-              activeOptions={activeOptions}
-              setActiveOptions={setActiveOptions}
-              searchesRef={searchesRef}
-            />
-
             <CardsContainer>
-              <Card town="" province="" temperature={1} rain={1} />
+              {ciudades.map(({ nombre }, index) => {
+                return (
+                  <Card
+                    key={index}
+                    town={nombre}
+                    province=""
+                    temperature={1}
+                    rain={1}
+                  />
+                );
+              })}
             </CardsContainer>
             <SignOut auth={auth} />
           </>
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Credits from '../../Credits/Component';
+import Credits from '../../Credits/Credits';
 import { H1 } from '../../styleguide/Typography';
 import * as styles from './styles';
 
