7f894f8ff13815331b9d9993f7cdbb28c52e7421
Miguel Hernanz <migherdcas@gmail.com>
2020-11-04 21:23:16 +0100

    feat(Constants): Cleanup and some explanation added.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,10 +1,19 @@
+/*
+    Town list
+    https://www.el-tiempo.net/api/json/v2/municipios
+
+    >>>>>>>>>>>> API DOESN'T WORK HERE <<<<<<<<<<<<<
+    Geographic and forecast data from a town
+    https://www.el-tiempo.net/api/json/v2/provincias/[CODPROV]/municipios/[ID]
+
+    Try: https://www.el-tiempo.net/api/json/v1/provincias/01/municipios/01001000000 (v1)
+    Try: https://www.el-tiempo.net/api/json/v2/provincias/01/municipios/01001000000 (v2)
+ */
+
 export const API_URL_LIST = 'https://www.el-tiempo.net/api/json/v2/municipios';
+export const API_URL_SEARCH = [
+  'https://api.openweathermap.org/data/2.5/weather?q=',
+  '&units=metric&appid=',
+];
 export const FIRESTORE_COLLECTION = 'municipios';
 export const FIRESTORE_LIST_DOCUMENT = 'lista';
-
-/*
-  Lista de municipios:
-    https://www.el-tiempo.net/api/json/v2/municipios
-  Información geográfica y meteorológica de un municipio
-    https://www.el-tiempo.net/api/json/v2/provincias/[CODPROV]/municipios/[ID]
- */
